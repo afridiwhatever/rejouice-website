4900138fdfacbf6e0d5b6ea609fa3b4315343659
Afridi <afridiwhatever@gmail.com>
2023-12-20 21:06:54 +0600

    Add moving cursor

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const heroSection = document.querySelector("#hero-section");
 
+const cursor = document.querySelector("#cursor");
+
+heroSection.addEventListener("mousemove", (event) => {
+  gsap.to(cursor, {
+    x: event.x,
+    y: event.y,
+  });
+});
